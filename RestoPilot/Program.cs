d97991274ad1902ebe657cf1512d800881cd012f
Ken-Andre <andre.kenmogne@2026.ucac-icam.com>
2023-12-19 11:22:29 +0100

    add sql_cre file

--- RestoPilot/Program.cs
+++ RestoPilot/Program.cs
@@ -2,9 +2,14 @@ using RestoPilot.Controller;
 
 namespace RestoPilot;
 
-static class Program {
-    /// <summary22   
+static class Program
+{
+    /// <summary>
     ///  The main entry point for the application.
     /// </summary>
-    [STAThread] static void Main() { new RestoController().Start(); }
+    [STAThread]
+    static void Main()
+    {
+        new RestoController().Start();
+    }
 }
